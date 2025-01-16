e98a101e71188ebff755e764b9ff8648d204d336
Daitoros <daviabmedeiros@gmail.com>
2025-01-16 12:08:33 -0300

    Notas sobre o restante do código

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,5 +1,6 @@
-import { Text, View } from 'react-native';
 import { styles } from './style';
+import { useState } from 'react';
+import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 
 export function Home(){
@@ -34,6 +35,54 @@ export function Home(){
             }
         ])
     }
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Compras pra Mãe
+            </Text>
+            <Text style={styles.text}>
+                Lista de Mercado
+            </Text>
+
+            <View style={styles.form}>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Adicione o item"
+                    placeholderTextColor="#6B6B6B"
+                    // onChangeText={texto => console.log(texto)}
+                    // onChangeText={texto => setNomeAluno(texto)}
+                    onChangeText={setNomeItem}                          //Atualiza o estado NomeItem ("setNomeItem" é usado para atualizar o estado NomeItem)
+                    value={nomeItem}                                    //Exibe o valor do campo atual
+                />
+
+                <TouchableOpacity style={styles.button}                 //Botão clicável. "onPress"(ao ser pressionado) aciona a função AddItem
+                                        onPress={AddItem} >         
+                    <Text style={styles.buttonText}>
+                        ➕
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <FlatList                                                   //Componente que renderiza listas
+                data={itens}                                            //Carrega a lista de itens, no estado itens
+                keyExtractor={(item) => item}                           //Define uma chave para cada item
+                renderItem={({ item })=> (                              //Define como cada item será renderizado
+                    <Itens
+                        key={item}
+                        name={item}                                     //nomeia os itens
+                        onRemove={() => RemoverItem(item)}              //ao remover o item, aciona a função removendo da lista. (O botão de remover está dentro de Itens)
+                    />
+                )}
+                showsVerticalScrollIndicator={false}                    //Remove a barra de rolagem lateral
+                ListEmptyComponent={() => (                             //O que exibir em caso de lista vazia
+                    <Text style={styles.listEmptyText}>
+                        Nenhum item adicionado
+                    </Text>
+                )}
+            />
+        </View>
+    )
+
 }
 
 
