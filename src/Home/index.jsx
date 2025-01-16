2a075e5b17be99efaf6e291c0f785b80abeb85f1
Daitoros <daviabmedeiros@gmail.com>
2025-01-16 10:12:42 -0300

    Primeira estudada no código modelo do professor, fazendo comentários e as devidas alterações para minhas necessidades

---
+++ src/Home/index.jsx
@@ -0,0 +1,25 @@
+import { Text, View } from 'react-native';
+import { styles } from './style';
+
+
+export function Home(){
+    // return (
+    //     <View style={styles.container}>
+    //         <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+    //     </View>
+    // )
+    const [itens, setItens ] = useState([]);        // Cria um array vazio de itens e o estabelece no react como um estado dinâmico. setItens será usado para adicionar itens no array.
+    const [nomeItem, setNomeItem] = useState('');    // Cria array de nome do item para ser preenchido pelo input.
+
+    function AddItem() {
+        
+        if (itens.includes(nomeItem)) {             // Checa se o item escrito no input já existe no array de itens
+            return Alert.alert("Item já existe", "Já existe um item na lista com esse nome.");
+          }
+      
+          setItens(prevState => [...prevState,nomeItem]);       // Isso pega a lista atual de itens e atualiza adicionando um novo item no final, o que está no input
+          setNomeItem('');                                      // Reseta o input, deixando em branco
+    }
+}
+
+
